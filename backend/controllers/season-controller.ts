fd6f5631acb1da1763bac244f190b98357dd0186
Alina Dakhno <alinad@hexaware.com>
2025-06-20 13:21:45 +0200

    remove router from controller

--- backend/controllers/season-controller.ts
+++ backend/controllers/season-controller.ts
@@ -2,9 +2,7 @@ import express from "express";
 import { handleServerError, notFound } from "../utils/errors.ts";
 import { getAllSeasons as getAllSeasonsService, getSeasonByYear } from "../services/season-service.ts";
 
-const router = express.Router();
-
-export const getAllSeasons = router.get("/", (async (_req, res) => {
+export const getAllSeasons = (async (_req, res) => {
     try {
         const seasons = await getAllSeasonsService();
         res.json(seasons);
@@ -12,9 +10,9 @@ export const getAllSeasons = router.get("/", (async (_req, res) => {
         console.error("Error fetching seasons:", err);
         handleServerError(res, err, "Error fetching seasons");
     }
-}) as express.RequestHandler);
+}) as express.RequestHandler;
 
-export const getSeason = router.get("/:season", (async (req, res) => {
+export const getSeason = (async (req, res) => {
     const { season } = req.params;
 
     try {
@@ -29,4 +27,4 @@ export const getSeason = router.get("/:season", (async (req, res) => {
         console.error("Error fetching season:", err);
         handleServerError(res, err, "Error fetching season");
     }
-}) as express.RequestHandler);
+}) as express.RequestHandler;
